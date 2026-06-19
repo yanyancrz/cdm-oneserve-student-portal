2e83b0176b2dce76b1fb05c5f851d9eab574f6da
cruzalhianne@gmail.com <cruzalhianne@gmail.com>
2026-06-20 00:44:05 +0800

    updated vite.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+    plugins: [
+        react(),
+
+        VitePWA({
+            registerType: "autoUpdate",
+
+            manifest: {
+                name: "CDM OneServe",
+                short_name: "OneServe",
+                description:
+                    "Integrated Campus Service Platform",
+
+                theme_color: "#106A2E",
+                background_color: "#F1F1F1",
+
+                display: "standalone",
+
+                start_url: "/",
+
+                icons: [
+                    {
+                        src: "/icons/icon-192.png",
+                        sizes: "192x192",
+                        type: "image/png"
+                    },
+                    {
+                        src: "/icons/icon-512.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    }
+                ]
+            }
+        })
+    ]
+});
